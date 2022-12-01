c5fbf3e26cfb03f82b92d2e2de120fdae1e453c4
rxdps93 <dpsirott@gmail.com>
2022-12-01 11:53:44 -0500

    day 1 puzzle 1 complete

---
+++ day01/puzzle1/d1p1.kt
@@ -0,0 +1,20 @@
+import java.io.File
+
+fun main(args : Array<String>) {
+    val fileName = "../input.txt"
+    val lines: List<String> = File(fileName).readLines()
+
+    var maxCals: Int = Int.MIN_VALUE
+    var sum: Int = 0
+    lines.forEach {
+        var cal = it.toIntOrNull()
+        if (cal != null) {
+            sum += cal
+        } else {
+            maxCals = if (sum > maxCals) sum else maxCals
+            sum = 0
+        }
+    }
+
+    println(maxCals)
+}
