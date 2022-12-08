93dfc294167e7df1cbaab20ca5362602f8088a02
rxdps93 <dpsirott@gmail.com>
2022-12-08 17:14:30 -0500

    d8p2 will finish later

--- day08/puzzle2/d8p2.kt
+++ day08/puzzle2/d8p2.kt
@@ -1,6 +1,17 @@
-package day08.puzzle1
+package day08.puzzle2
 
 import java.io.File
+
+fun printGrid(grid: Array<BooleanArray>) {
+
+    for (row in grid) {
+        for (tree in row) {
+            print(if (tree) "T" else "F")
+        }
+        println()
+    }
+    println()
+}
 
 fun main(args : Array<String>) {
     val fileName = "day08/input.txt"
@@ -9,14 +20,14 @@ fun main(args : Array<String>) {
     val rows = lines.size
     val cols = lines[0].length
 
-    val grid = Array(rows) { BooleanArray(cols) }
+    val grid = Array(rows) { IntArray(cols) }
 
     // Mark Outsides
-    grid[0] = BooleanArray(cols) { true }
-    grid[rows - 1] = BooleanArray(cols) { true }
+    grid[0] = IntArray(cols) { 0 }
+    grid[rows - 1] = IntArray(cols) { 0 }
     for (row in grid) {
-        row[0] = true
-        row[row.size - 1] = true
+        row[0] = 0
+        row[row.size - 1] = 0
     }
 
     // Visible From Left
@@ -25,7 +36,7 @@ fun main(args : Array<String>) {
         for (col in 1 until cols - 1) {
             if (lines[row][col].digitToInt() > max) {
                 max = lines[row][col].digitToInt()
-                grid[row][col] = true
+                grid[row][col] = 0
             }
         }
     }
@@ -36,7 +47,7 @@ fun main(args : Array<String>) {
         for (col in cols - 1 downTo 1) {
             if (lines[row][col].digitToInt() > max) {
                 max = lines[row][col].digitToInt()
-                grid[row][col] = true
+                grid[row][col] = 0
             }
         }
     }
@@ -47,7 +58,7 @@ fun main(args : Array<String>) {
         for (row in 1 until rows - 1) {
             if (lines[row][col].digitToInt() > max) {
                 max = lines[row][col].digitToInt()
-                grid[row][col] = true
+                grid[row][col] = 0
             }
         }
     }
@@ -58,10 +69,10 @@ fun main(args : Array<String>) {
         for (row in cols - 1 downTo 1) {
             if (lines[row][col].digitToInt() > max) {
                 max = lines[row][col].digitToInt()
-                grid[row][col] = true
+                grid[row][col] = 0
             }
         }
     }
 
-    println("There are ${grid.sumOf { it -> it.count { it } }} trees visible");
+//    println("There are ${grid.sumOf { it -> it.count { it } }} trees visible");
 }
