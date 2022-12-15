79ce9cf6fc255e3edde1da546aef4c3183f7c57d
rxdps93 <dpsirott@gmail.com>
2022-12-15 10:29:02 -0500

    day 14 puzzle 1 complete

--- README.md
+++ README.md
@@ -44,7 +44,7 @@ I'm doing it in Kotlin.
   - [x] puzzle 1
   - [x] puzzle 2
 - [ ] Day 14
-  - [ ] puzzle 1
+  - [x] puzzle 1
   - [ ] puzzle 2
 - [ ] Day 15
   - [ ] puzzle 1
--- day14/puzzle1/d14p1.kt
+++ day14/puzzle1/d14p1.kt
@@ -4,10 +4,48 @@ import java.io.File
 import kotlin.math.max
 import kotlin.math.min
 
+fun printGrid(grid: Array<CharArray>) {
+    for (row in grid) {
+        for (col in row) {
+            print(col)
+        }
+        println()
+    }
+}
+fun dropSand(grid: Array<CharArray>, xrange: IntRange, ymax: Int): Int {
+
+    var sand = 0
+    var sandCoords = Pair(500 - xrange.first, 0)
+    while (true) {
+
+        try {
+            if (grid[sandCoords.second + 1][sandCoords.first] == '.') { // move down
+                sandCoords = sandCoords.copy(second = sandCoords.second + 1)
+            } else if (grid[sandCoords.second + 1][sandCoords.first - 1] == '.') { // move down/left
+                sandCoords = sandCoords.copy(second = sandCoords.second + 1, first = sandCoords.first - 1)
+            } else if (grid[sandCoords.second + 1][sandCoords.first + 1] == '.') { // move down/right
+                sandCoords = sandCoords.copy(second = sandCoords.second + 1, first = sandCoords.first + 1)
+            } else { // come to rest
+                grid[sandCoords.second][sandCoords.first] = 'o'
+                sand++
+                sandCoords = Pair(500 - xrange.first, 0)
+            }
+        } catch (e: ArrayIndexOutOfBoundsException) {
+            break
+        }
+
+        if (sandCoords.second > ymax) {
+            break
+        }
+    }
+
+    return sand
+}
+
 fun main() {
 
     var pairs = mutableListOf<Pair<Pair<Int, Int>, Pair<Int, Int>>>()
-    File("day14/testInput.txt").forEachLine {
+    File("day14/input.txt").forEachLine {
         var leftCoord = Pair(-1, -1)
         for (coord in it.filterNot { it.isWhitespace() }.split("->")) {
             var pair = coord.split(",")
@@ -26,13 +64,16 @@ fun main() {
     var ymax = pairs.maxOf { max(it.first.second, it.second.second) }
 
     var grid: Array<CharArray> = Array(ymax + 1) { CharArray((xmax - xmin) + 1) { '.' } }
+    for (pair in pairs) {
+        for (y in min(pair.first.second, pair.second.second).. max(pair.first.second, pair.second.second)) {
+            for (x in (min(pair.first.first, pair.second.first) - xmin)..(max(pair.first.first, pair.second.first) - xmin)) {
+                grid[y][x] = '#'
+            }
+        }
+    }
 
-//    for (row in grid) {
-//        for (col in row) {
-//            print(col)
-//        }
-//        println()
-//    }
+    grid[0][500 - xmin] = '+'
 
-//    println("x_min=$xmin, x_max=$xmax, y_max=$ymax")
+    println("A total of ${dropSand(grid, xmin..xmax, ymax)} units of sand can come to rest.")
+//    printGrid(grid)
 }
