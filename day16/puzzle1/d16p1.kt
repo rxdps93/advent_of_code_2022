a13def7b05ac725985d15e04111dc9da7cfd1795
rxdps93 <dpsirott@gmail.com>
2022-12-17 22:18:00 -0500

    closer

--- day16/puzzle1/d16p1.kt
+++ day16/puzzle1/d16p1.kt
@@ -7,40 +7,48 @@ import java.io.File
 fun main() {
 
     val valves = hashMapOf<String, Valve>()
-    File("day16/testInput.txt").forEachLine { line->
+    File("day16/input.txt").forEachLine { line->
         val tokens = Regex("Valve ([A-Z]{2}) has flow rate=([0-9]+); tunnels? leads? to valves? ([A-Z, ]*)").find(line)!!
         valves[tokens.groupValues[1]] = Valve(tokens.groupValues[1], tokens.groupValues[2].toInt(), tokens.groupValues[3].split(", "))
     }
 
+    val workingValves = hashSetOf<Valve>()
     for (valve in valves.values) {
+        if (valve.flowRate > 0) {
+            workingValves.add(valve)
+        }
+
         for (str in valve.adjacent) {
             valve.tunnels.add(valves[str]!!)
         }
     }
 
-    val stack: ArrayDeque<Path> = ArrayDeque()
-    stack.addFirst(Path(valves["AA"]!!, arrayListOf(), 30, 0))
+    for (valve in valves.values) {
+        valve.generateRoutes(workingValves)
+    }
+
+    val queue: ArrayDeque<Path> = ArrayDeque()
+    queue.addLast(Path(valves["AA"]!!, arrayListOf(valves["AA"]!!), 30, 0))
     var best = Int.MIN_VALUE
-    while (stack.isNotEmpty()) {
-        var path = stack.removeFirst()
+    while (queue.isNotEmpty()) {
+        var path = queue.removeFirst()
 
         var end = true
-        for (valve in path.curValve.tunnels) {
-            if (path.remaining - 1 > 0 && !path.openValves.contains(valve)) {
+        for (route in path.curValve.routes.entries) {
+            var dist = route.value
+            var valve = route.key
+            if (path.remaining - dist > 0 && !path.openValves.contains(valve)) {
+                end = false
                 var open = ArrayList(path.openValves)
                 open.add(valve)
-                var pressure = (path.remaining - 1) * valve.flowRate
-//                System.out.println("at " + state + " using way " + target + "/" + dist + " freshly releasing " + newlyReleased);
-                println("at ${path.curValve.name}, ${path.remaining}, ${path.openValves}, ${path.pressureReleased}")
-                stack.addFirst(Path(valve, open, path.remaining - 1, path.pressureReleased + pressure))
-                end = false
+                var pressure = (path.remaining - dist) * valve.flowRate
+                queue.addLast(Path(valve, open, path.remaining - dist, path.pressureReleased + pressure))
             }
         }
 
-        if (end) {
-            if (path.pressureReleased > best) {
-                best = path.pressureReleased
-            }
+        if (end && path.pressureReleased > best) {
+            println("new best: ${path.curValve}, ${path.remaining}, ${path.pressureReleased}")
+            best = path.pressureReleased
         }
     }
 
--- day16/util/Valve.kt
+++ day16/util/Valve.kt
@@ -1,5 +1,34 @@
 package day16.util
 
 class Valve(val name: String, val flowRate: Int, val adjacent: List<String>) {
-    val tunnels: MutableList<Valve> = mutableListOf()
+    val tunnels = mutableListOf<Valve>()
+    val routes = hashMapOf<Valve, Int>()
+
+    fun generateRoutes(valves: HashSet<Valve>) {
+        val queue = ArrayDeque<Valve>()
+        val visited = hashSetOf<Valve>()
+        val dists = hashMapOf<Valve, Int>()
+        queue.addLast(this)
+        dists[this] = 0
+        while (queue.isNotEmpty() && routes.size < valves.size) {
+            val valve = queue.removeFirst()
+            visited.add(valve)
+            val dist = dists[valve]!!
+
+            if (valve != this && valves.contains(valve)) {
+                routes[valve] = dist + 1
+            }
+
+            for (adj in valve.tunnels) {
+                if (!visited.contains(adj)) {
+                    queue.addLast(adj)
+                    dists[adj] = dist + 1
+                }
+            }
+        }
+    }
+
+    override fun toString(): String {
+        return this.name
+    }
 }
