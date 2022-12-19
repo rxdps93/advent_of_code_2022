75b121dff8a86b51328f3da5061bbe9db56b289c
rxdps93 <dpsirott@gmail.com>
2022-12-19 08:54:02 -0500

    trying a recursive approach instead

--- day16/puzzle1/d16p1.kt
+++ day16/puzzle1/d16p1.kt
@@ -4,6 +4,26 @@ import day16.util.Path
 import day16.util.Valve
 import java.io.File
 
+fun openValves(remaining: Int,
+               curValve: Valve,
+               valves: HashMap<String, Valve>,
+               targetValves: Set<Valve>,
+               paths: HashMap<Path, Int> = hashMapOf()
+): Int {
+
+    val pressureReleased = remaining * curValve.flowRate
+    val path = Path(curValve, targetValves, remaining)
+
+    paths.getOrPut(path) {
+        targetValves.filter { target-> curValve.routes[target]!! < remaining }
+            .takeIf { it.isNotEmpty() }?.maxOf { target->
+                val time = remaining - 1 - curValve.routes[target]!!
+                openValves(time, target, valves,targetValves - target, paths)
+            }?: 0
+    }
+
+}
+
 fun main() {
 
     val valves = hashMapOf<String, Valve>()
@@ -12,42 +32,44 @@ fun main() {
         valves[tokens.groupValues[1]] = Valve(tokens.groupValues[1], tokens.groupValues[2].toInt(), tokens.groupValues[3].split(", "))
     }
 
-    val workingValves = hashSetOf<Valve>()
     for (valve in valves.values) {
-        if (valve.flowRate > 0) {
-            workingValves.add(valve)
-        }
 
         for (str in valve.adjacent) {
             valve.tunnels.add(valves[str]!!)
         }
     }
 
+    val workingValves: HashSet<Valve> = valves.values.filter { it.flowRate > 0 }.toHashSet()
+
     for (valve in valves.values) {
         valve.generateRoutes(workingValves)
     }
 
-    val queue: ArrayDeque<Path> = ArrayDeque()
-    queue.addLast(Path(valves["AA"]!!, arrayListOf(valves["AA"]!!), 30, 0))
-    var best = Int.MIN_VALUE
-    while (queue.isNotEmpty()) {
-        var path = queue.removeFirst()
+    println("Maximum pressure released in 30 minutes is: ${openValves(30, valves["AA"]!!, valves, workingValves)}")
 
-        if (path.pressureReleased > best) {
-            best = path.pressureReleased
-        }
 
-        for (route in path.curValve.routes.entries) {
-            var dist = route.value
-            var valve = route.key
-            if (path.remaining > 0 && !path.openValves.contains(valve)) {
-                var open = ArrayList(path.openValves)
-                open.add(valve)
-                var pressure = (path.remaining - dist) * valve.flowRate
-                queue.addLast(Path(valve, open, path.remaining - dist, path.pressureReleased + pressure))
-            }
-        }
-    }
-
-    println("Maximum pressure released in 30 minutes is: $best")
+//    val queue: ArrayDeque<Path> = ArrayDeque()
+//    queue.addLast(Path(valves["AA"]!!, arrayListOf(valves["AA"]!!), 30, 0))
+//    var best = Int.MIN_VALUE
+//    while (queue.isNotEmpty()) {
+//        var path = queue.removeFirst()
+//
+//        if (path.pressureReleased > best) {
+//            println(path.pressureReleased)
+//            best = path.pressureReleased
+//        }
+//
+//        for (route in path.curValve.routes.entries) {
+//            var dist = route.value
+//            var valve = route.key
+//            if (path.remaining - dist >= 0 && !path.openValves.contains(valve)) {
+//                var open = ArrayList(path.openValves)
+//                open.add(valve)
+//                var pressure = (path.remaining - dist) * valve.flowRate
+//                queue.addLast(Path(valve, open, path.remaining - dist, path.pressureReleased + pressure))
+//            }
+//        }
+//    }
+//
+//    println("Maximum pressure released in 30 minutes is: $best")
 }
--- day16/util/Path.kt
+++ day16/util/Path.kt
@@ -1,3 +1,4 @@
 package day16.util
 
-class Path(var curValve: Valve, val openValves: ArrayList<Valve>, var remaining: Int, var pressureReleased: Int)
+//class Path(var curValve: Valve, val openValves: ArrayList<Valve>, var remaining: Int, var pressureReleased: Int)
+class Path(var curValve: Valve, val targetValves: Set<Valve>, val remaining: Int)
