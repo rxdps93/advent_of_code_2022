e088277c04b716358f95bd32475a53c3953d0ef0
rxdps93 <dpsirott@gmail.com>
2023-01-10 14:00:23 -0500

    day 19 puzzle 1 complete

--- README.md
+++ README.md
@@ -59,7 +59,7 @@ I'm doing it in Kotlin.
   - [x] puzzle 1
   - [x] puzzle 2
 - [ ] Day 19
-  - [ ] puzzle 1
+  - [x] puzzle 1
   - [ ] puzzle 2
 - [ ] Day 20
   - [ ] puzzle 1
--- day19/puzzle1/d19p1.kt
+++ day19/puzzle1/d19p1.kt
@@ -1,31 +1,94 @@
 package day19.puzzle1
 
 import java.io.File
+import kotlin.system.measureNanoTime
 
-enum class Robot(val max: Int) {
-    ORE(2),
-    CLAY(4),
-    OBSIDIAN(4),
-    GEODE(Int.MAX_VALUE)
-}
+private data class State(
+        var minutes: Int = 24,
+        var oreBots: Int = 1, var clayBots: Int = 0, var obsBots: Int = 0, var geoBots: Int = 0,
+        var ore: Int = 0, var clay: Int = 0, var obsidian: Int = 0, var geodes: Int = 0)
 
-class Blueprint(val id: Int, var best: Int,
+private class Blueprint(val id: Int, var best: Int,
                 val oreBotCost: Int, val clayBotCost: Int,
                 val obsBotCost: Pair<Int, Int>, val geoBotCost: Pair<Int, Int>) {
 
-    fun canAfford(ore: Int, clay: Int, obsidian: Int, robot: Robot): Boolean {
-        return when (robot) {
-            Robot.ORE -> {
-                ore >= oreBotCost
+    private val oreMax = maxOf(this.oreBotCost, this.clayBotCost, this.obsBotCost.first, this.geoBotCost.first)
+
+    fun simulate() {
+
+        val queue = ArrayDeque<State>()
+        queue.addLast(State())
+        val visited = hashSetOf<State>()
+
+        while (queue.isNotEmpty()) {
+            var state = queue.removeFirst()
+
+            if (visited.contains(state)) {
+                continue
             }
-            Robot.CLAY -> {
-                ore >= clayBotCost
+
+            visited.add(state)
+
+            if (state.minutes <= 0) {
+                this.best = maxOf(this.best, state.geodes)
+                continue
             }
-            Robot.OBSIDIAN -> {
-                (ore >= obsBotCost.first) && (clay >= obsBotCost.second)
+
+            // just collect resources
+            queue.addLast(state.copy(
+                    minutes = state.minutes - 1,
+                    ore = state.ore + state.oreBots,
+                    clay = state.clay + state.clayBots,
+                    obsidian = state.obsidian + state.obsBots,
+                    geodes = state.geodes + state.geoBots
+            ))
+
+            // buy orebot
+            if (state.ore >= this.oreBotCost && state.oreBots < this.oreMax) {
+                queue.addLast(state.copy(
+                        minutes = state.minutes - 1,
+                        ore = state.ore - this.oreBotCost + state.oreBots,
+                        clay = state.clay + state.clayBots,
+                        obsidian = state.obsidian + state.obsBots,
+                        geodes = state.geodes + state.geoBots,
+                        oreBots = state.oreBots + 1
+                ))
             }
-            Robot.GEODE -> {
-                (ore >= geoBotCost.first) && (obsidian >= geoBotCost.second)
+
+            // buy claybot
+            if (state.ore >= this.clayBotCost && state.clayBots < this.obsBotCost.second) {
+                queue.addLast(state.copy(
+                        minutes = state.minutes - 1,
+                        ore = state.ore - this.clayBotCost + state.oreBots,
+                        clay = state.clay + state.clayBots,
+                        obsidian = state.obsidian + state.obsBots,
+                        geodes = state.geodes + state.geoBots,
+                        clayBots = state.clayBots + 1
+                ))
+            }
+
+            // buy obsbot
+            if (state.ore >= this.obsBotCost.first && state.clay >= this.obsBotCost.second && state.obsBots < this.geoBotCost.second) {
+                queue.addLast(state.copy(
+                        minutes = state.minutes - 1,
+                        ore = state.ore - this.obsBotCost.first + state.oreBots,
+                        clay = state.clay - this.obsBotCost.second + state.clayBots,
+                        obsidian = state.obsidian + state.obsBots,
+                        geodes = state.geodes + state.geoBots,
+                        obsBots = state.obsBots + 1
+                ))
+            }
+
+            // buy geobot
+            if (state.ore >= this.geoBotCost.first && state.obsidian >= this.geoBotCost.second) {
+                queue.addLast(state.copy(
+                        minutes = state.minutes - 1,
+                        ore = state.ore - this.geoBotCost.first + state.oreBots,
+                        clay = state.clay + state.clayBots,
+                        obsidian = state.obsidian - this.geoBotCost.second + state.obsBots,
+                        geodes = state.geodes + state.geoBots,
+                        geoBots = state.geoBots + 1
+                ))
             }
         }
     }
@@ -43,100 +106,17 @@ class Blueprint(val id: Int, var best: Int,
     }
 }
 
-fun simulate(bp: Blueprint) {
-    var minutes = 1
-    var botCount = hashMapOf<Robot, Int>()
-    botCount[Robot.ORE] = 1
-    botCount[Robot.CLAY] = 0
-    botCount[Robot.OBSIDIAN] = 0
-    botCount[Robot.GEODE] = 0
-    var ore = 0
-    var clay = 0
-    var obsidian = 0
-    var geodes = 0
-    val robots = arrayOf(Robot.GEODE, Robot.CLAY, Robot.OBSIDIAN, Robot.ORE)
-    while (minutes <= 24) {
-        // try to begin construction a robot
-        for (robot in robots) {
-            if (botCount[robot]!! < robot.max && bp.canAfford(ore, clay, obsidian, robot)) {
-                botCount[robot] = botCount[robot]!! + 1
-                when (robot) {
-                    Robot.ORE -> { ore -= bp.oreBotCost }
-                    Robot.CLAY -> { ore -= bp.clayBotCost }
-                    Robot.OBSIDIAN -> { ore -= bp.obsBotCost.first; clay -= bp.obsBotCost.second }
-                    Robot.GEODE -> { ore -= bp.geoBotCost.first; obsidian -= bp.geoBotCost.second }
-                }
-
-                println("Built a robot: $robot")
-                break
-            }
-        }
-
-
-        ore += botCount[Robot.ORE]!!
-        println("${botCount[Robot.ORE]} ore-collecting robots collects ${botCount[Robot.ORE]}; you now have $ore ore.")
-
-        minutes++
-    }
-}
-
 fun main() {
-    val input = File("day19/testInput.txt").readLines().map {  line ->
+    val input = File("day19/input.txt").readLines().map {  line ->
         val tokens = Regex("Blueprint ([0-9]*): Each ore robot costs ([0-9]*) ore. Each clay robot costs ([0-9]*) ore. Each obsidian robot costs ([0-9]*) ore and ([0-9]*) clay. Each geode robot costs ([0-9]*) ore and ([0-9]*) obsidian.").find(line)!!.groupValues
         Blueprint(tokens[1].toInt(), Int.MIN_VALUE,
                 tokens[2].toInt(), tokens[3].toInt(),
                 Pair(tokens[4].toInt(), tokens[5].toInt()), Pair(tokens[6].toInt(), tokens[7].toInt()))
     }
 
-    input.forEach { println(it) }
-
-    input.forEach { simulate(it) }
+    input.forEach {
+        it.simulate()
+    }
 
     println("The sum of the quality levels is ${input.sumOf { it.qualityLevel() }}")
-
-//    val oreBotMetrics = Triple(input.minOf { it.oreBotCost }, input.sumOf { it.oreBotCost } / input.size, input.maxOf { it.oreBotCost })
-//    val clayBotMetrics = Triple(input.minOf { it.clayBotCost }, input.sumOf { it.clayBotCost } / input.size, input.maxOf { it.clayBotCost })
-//    val obsBotMetrics = Triple(
-//            Pair(input.minOf { it.obsBotCost.first }, input.minOf { it.obsBotCost.second }),
-//            Pair(input.sumOf { it.obsBotCost.first } / input.size, input.sumOf { it.obsBotCost.second } / input.size),
-//            Pair(input.maxOf { it.obsBotCost.first }, input.maxOf { it.obsBotCost.second }))
-//    val geoBotMetrics = Triple(
-//            Pair(input.minOf { it.geoBotCost.first }, input.minOf { it.geoBotCost.second }),
-//            Pair(input.sumOf { it.geoBotCost.first } / input.size, input.sumOf { it.geoBotCost.second } / input.size),
-//            Pair(input.maxOf { it.geoBotCost.first }, input.maxOf { it.geoBotCost.second }))
-//    val oreMetrics = Triple(
-//            input.minOf { minOf(it.oreBotCost, it.clayBotCost, it.obsBotCost.first, it.geoBotCost.first) },
-//            input.sumOf { (it.oreBotCost + it.clayBotCost + it.obsBotCost.first + it.geoBotCost.first) / 4 } / input.size,
-//            input.maxOf { maxOf(it.oreBotCost, it.clayBotCost, it.obsBotCost.first, it.geoBotCost.first) })
-//    val clayMetrics = Triple(
-//            input.minOf { it.obsBotCost.second },
-//            input.sumOf { it.obsBotCost.second } / input.size,
-//            input.maxOf { it.obsBotCost.second })
-//    val obsMetrics = Triple(
-//            input.minOf { it.geoBotCost.second },
-//            input.sumOf { it.geoBotCost.second } / input.size,
-//            input.maxOf { it.geoBotCost.second })
-//
-//    println("+----------+----------------+----------------+----------------+")
-//    println("|  Metric  |       Min      |       Avg      |       Max      |")
-//    println("+----------+----------------+----------------+----------------+")
-//    println("| OreBot   | %2d ore         | %2d ore         | %2d ore         |"
-//            .format(oreBotMetrics.first, oreBotMetrics.second, oreBotMetrics.third))
-//    println("| ClayBot  | %2d ore         | %2d ore         | %2d ore         |"
-//            .format(clayBotMetrics.first, clayBotMetrics.second, clayBotMetrics.third))
-//    println("| ObsBot   | %2d ore %2d clay | %2d ore %2d clay | %2d ore %2d clay |"
-//            .format(obsBotMetrics.first.first, obsBotMetrics.first.second,
-//                    obsBotMetrics.second.first, obsBotMetrics.second.second,
-//                    obsBotMetrics.third.first, obsBotMetrics.third.second))
-//    println("| GeoBot   | %2d ore %2d obs  | %2d ore %2d obs  | %2d ore %2d obs  |"
-//            .format(geoBotMetrics.first.first, geoBotMetrics.first.second,
-//                    geoBotMetrics.second.first, geoBotMetrics.second.second,
-//                    geoBotMetrics.third.first, geoBotMetrics.third.second))
-//    println("| Ore      | %2d             | %2d             | %2d             |"
-//            .format(oreMetrics.first, oreMetrics.second, oreMetrics.third))
-//    println("| Clay     | %2d             | %2d             | %2d             |"
-//            .format(clayMetrics.first, clayMetrics.second, clayMetrics.third))
-//    println("| Obs      | %2d             | %2d             | %2d             |"
-//            .format(obsMetrics.first, obsMetrics.second, obsMetrics.third))
-//    println("+----------+----------------+----------------+----------------+")
 }
